cc5de2d0fefb91219df00152d78d8ef2dbf0bd4f
Erin Millard <contact@erin.id.au>
2023-07-20 18:16:01 +1000

    Refactor Permissions tests

--- test/jest/Permissions.spec.ts
+++ test/jest/Permissions.spec.ts
@@ -1,10 +1,6 @@
 import * as defaultPermissionNames from "../../src/constants/permission-name.js";
 import { Permissions, PermissionStatus } from "../../src/index.js";
 
-const defaultPermissionNamesData: [PermissionName][] = Object.values(
-  defaultPermissionNames,
-).map((name) => [name]) as [PermissionName][];
-
 describe("Permissions", () => {
   let permissions: Permissions;
   let callQueryWith: (...a: unknown[]) => () => Promise<unknown>;
@@ -19,116 +15,115 @@ describe("Permissions", () => {
     };
   });
 
-  describe("query()", () => {
-    describe("when called without arguments", () => {
-      it("throws a TypeError", async () => {
-        const call = callQueryWith();
+  describe("when queried without arguments", () => {
+    it("throws a TypeError", async () => {
+      const call = callQueryWith();
 
-        await expect(call).rejects.toThrow(TypeError);
-        await expect(call).rejects.toThrow(
-          "Failed to execute 'query' on 'Permissions': 1 argument required, but only 0 present.",
-        );
-      });
-    });
-
-    describe("when called with a non-object descriptor", () => {
-      it("throws a TypeError", async () => {
-        const call = callQueryWith(null);
-
-        await expect(call).rejects.toThrow(TypeError);
-        await expect(call).rejects.toThrow(
-          "Failed to execute 'query' on 'Permissions': parameter 1 is not of type 'object'.",
-        );
-      });
-    });
-
-    describe("when called with an empty object descriptor", () => {
-      it("throws a TypeError", async () => {
-        const call = callQueryWith({});
-
-        await expect(call).rejects.toThrow(TypeError);
-        await expect(call).rejects.toThrow(
-          "Failed to execute 'query' on 'Permissions': Failed to read the 'name' property from 'PermissionDescriptor': Required member is undefined.",
-        );
-      });
-    });
-
-    describe("when called with a non-string permission name", () => {
-      it("throws a TypeError", async () => {
-        const call = callQueryWith({ name: null });
-
-        await expect(call).rejects.toThrow(TypeError);
-        await expect(call).rejects.toThrow(
-          "Failed to execute 'query' on 'Permissions': Failed to read the 'name' property from 'PermissionDescriptor': The provided value 'null' is not a valid enum value of type PermissionName.",
-        );
-      });
-    });
-
-    describe("when called with an empty string permission name", () => {
-      it("throws a TypeError", async () => {
-        const call = callQueryWith({ name: "" });
-
-        await expect(call).rejects.toThrow(TypeError);
-        await expect(call).rejects.toThrow(
-          "Failed to execute 'query' on 'Permissions': Failed to read the 'name' property from 'PermissionDescriptor': The provided value '' is not a valid enum value of type PermissionName.",
-        );
-      });
-    });
-
-    describe("when called with a non-existent permission name", () => {
-      it("throws a TypeError", async () => {
-        const call = callQueryWith({ name: "nonexistent" });
-
-        await expect(call).rejects.toThrow(TypeError);
-        await expect(call).rejects.toThrow(
-          "Failed to execute 'query' on 'Permissions': Failed to read the 'name' property from 'PermissionDescriptor': The provided value 'nonexistent' is not a valid enum value of type PermissionName.",
-        );
-      });
-    });
-
-    describe("when using the default permission name set", () => {
-      describe.each(defaultPermissionNamesData)(
-        "when called with a permission name of '%s'",
-        (name) => {
-          it("returns a status", async () => {
-            expect(await permissions.query({ name })).toBeInstanceOf(
-              PermissionStatus,
-            );
-          });
-        },
+      await expect(call).rejects.toThrow(TypeError);
+      await expect(call).rejects.toThrow(
+        "Failed to execute 'query' on 'Permissions': 1 argument required, but only 0 present.",
       );
     });
+  });
 
-    describe("when using a custom permission name set", () => {
-      const permissionNameA = "permission-name-a" as PermissionName;
-      const permissionNameB = "permission-name-b" as PermissionName;
+  describe("when queried with a non-object descriptor", () => {
+    it("throws a TypeError", async () => {
+      const call = callQueryWith(null);
 
-      beforeEach(() => {
-        permissions = new Permissions({
-          permissionNames: new Set([permissionNameA, permissionNameB]),
-        });
+      await expect(call).rejects.toThrow(TypeError);
+      await expect(call).rejects.toThrow(
+        "Failed to execute 'query' on 'Permissions': parameter 1 is not of type 'object'.",
+      );
+    });
+  });
+
+  describe("when queried with an empty object descriptor", () => {
+    it("throws a TypeError", async () => {
+      const call = callQueryWith({});
+
+      await expect(call).rejects.toThrow(TypeError);
+      await expect(call).rejects.toThrow(
+        "Failed to execute 'query' on 'Permissions': Failed to read the 'name' property from 'PermissionDescriptor': Required member is undefined.",
+      );
+    });
+  });
+
+  describe("when queried with a non-string permission name", () => {
+    it("throws a TypeError", async () => {
+      const call = callQueryWith({ name: null });
+
+      await expect(call).rejects.toThrow(TypeError);
+      await expect(call).rejects.toThrow(
+        "Failed to execute 'query' on 'Permissions': Failed to read the 'name' property from 'PermissionDescriptor': The provided value 'null' is not a valid enum value of type PermissionName.",
+      );
+    });
+  });
+
+  describe("when queried with an empty string permission name", () => {
+    it("throws a TypeError", async () => {
+      const call = callQueryWith({ name: "" });
+
+      await expect(call).rejects.toThrow(TypeError);
+      await expect(call).rejects.toThrow(
+        "Failed to execute 'query' on 'Permissions': Failed to read the 'name' property from 'PermissionDescriptor': The provided value '' is not a valid enum value of type PermissionName.",
+      );
+    });
+  });
+
+  describe("when using the default permission name set", () => {
+    describe("when queried with a permission name not in the set", () => {
+      it("throws a TypeError", async () => {
+        const call = callQueryWith({ name: "non-existent" });
+
+        await expect(call).rejects.toThrow(TypeError);
+        await expect(call).rejects.toThrow(
+          "Failed to execute 'query' on 'Permissions': Failed to read the 'name' property from 'PermissionDescriptor': The provided value 'non-existent' is not a valid enum value of type PermissionName.",
+        );
       });
+    });
 
-      describe("when called with a permission name in the custom set", () => {
-        it("returns statuses", async () => {
-          expect(
-            await permissions.query({ name: permissionNameA }),
-          ).toBeInstanceOf(PermissionStatus);
-          expect(
-            await permissions.query({ name: permissionNameB }),
-          ).toBeInstanceOf(PermissionStatus);
-        });
-      });
-
-      describe("when called with a permission name not in the custom set", () => {
-        it("throws a TypeError", async () => {
-          const call = callQueryWith({ name: "nonexistent" });
-
-          await expect(call).rejects.toThrow(TypeError);
-          await expect(call).rejects.toThrow(
-            "Failed to execute 'query' on 'Permissions': Failed to read the 'name' property from 'PermissionDescriptor': The provided value 'nonexistent' is not a valid enum value of type PermissionName.",
+    describe("when queried with a permission name in the set", () => {
+      it("returns a status", async () => {
+        for (const name of Object.values(
+          defaultPermissionNames,
+        ) as PermissionName[]) {
+          expect(await permissions.query({ name })).toBeInstanceOf(
+            PermissionStatus,
           );
-        });
+        }
+      });
+    });
+  });
+
+  describe("when using a custom permission name set", () => {
+    const permissionNameA = "permission-name-a" as PermissionName;
+    const permissionNameB = "permission-name-b" as PermissionName;
+
+    beforeEach(() => {
+      permissions = new Permissions({
+        permissionNames: new Set([permissionNameA, permissionNameB]),
+      });
+    });
+
+    describe("when queried with a permission name not in the set", () => {
+      it("throws a TypeError", async () => {
+        const call = callQueryWith({ name: "non-existent" });
+
+        await expect(call).rejects.toThrow(TypeError);
+        await expect(call).rejects.toThrow(
+          "Failed to execute 'query' on 'Permissions': Failed to read the 'name' property from 'PermissionDescriptor': The provided value 'non-existent' is not a valid enum value of type PermissionName.",
+        );
+      });
+    });
+
+    describe("when queried with a permission name in the set", () => {
+      it("returns a status", async () => {
+        expect(
+          await permissions.query({ name: permissionNameA }),
+        ).toBeInstanceOf(PermissionStatus);
+        expect(
+          await permissions.query({ name: permissionNameB }),
+        ).toBeInstanceOf(PermissionStatus);
       });
     });
   });
