38775655ca82f5e028491aab09094dbba12fab55
Erin Millard <contact@erin.id.au>
2023-07-21 21:36:14 +1000

    Extend EventTarget to implement event methods

--- src/permission-status.ts
+++ src/permission-status.ts
@@ -1,29 +1,19 @@
 import { PermissionStore } from "./permission-store.js";
 import { StdPermissionStatus } from "./types/std.js";
 
-export class PermissionStatus<Name extends string> {
+export class PermissionStatus<Name extends string> extends EventTarget {
   readonly name: Name;
   onchange: ((ev: Event) => void) | null = null;
 
   constructor(permissionStore: PermissionStore<Name>, name: Name) {
+    super();
+
     this.#permissionStore = permissionStore;
     this.name = name;
   }
 
   get state(): PermissionState {
     return this.#permissionStore.get(this.name);
-  }
-
-  addEventListener(): void {
-    throw new Error("Not implemented");
-  }
-
-  removeEventListener(): void {
-    throw new Error("Not implemented");
-  }
-
-  dispatchEvent(): boolean {
-    throw new Error("Not implemented");
   }
 
   readonly #permissionStore: PermissionStore<Name>;
