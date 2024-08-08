487829bb183960281952d5b3bd146dc6441cafb6
Erin Millard <contact@erin.id.au>
2024-08-08 11:15:34 +1000

    Shorten test timeouts

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,9 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
+    testTimeout: 1000,
+    hookTimeout: 1000,
+    teardownTimeout: 1000,
     include: ["test/vitest/**/*.spec.ts"],
     coverage: {
       include: ["src/**/*.ts"],
